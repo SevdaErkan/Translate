fb4e6d69fb6b0076ea5db3a68e1b2d45c3df7035
SevdaErkan <51799297+SevdaErkan@users.noreply.github.com>
2021-07-06 22:17:04 -0700

    Initial commit

---
+++ src/components/translate.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+require("dotenv").config();
+
+const doTranslation = async (input, languageCode, cancelToken) => {
+  try {
+    const { data } = await axios.post(
+      "https://translation.googleapis.com/language/translate/v2?key=process.env.REACT_API_KEY",
+      {
+        q: input,
+        target: languageCode
+      },
+      { cancelToken: cancelToken.token }
+    );
+
+    return data.data.translations[0].translatedText;
+  } catch (err) {
+    return "";
+  }
+};
+
+export default ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
+  // console.log(process.env.REACT_API_KEY)
+
+  useEffect(() => {
+    if (!text) {
+      return;
+    }
+
+    const cancelToken = axios.CancelToken.source();
+
+    doTranslation(text, language, cancelToken).then(setTranslated);
+
+    return () => {
+      try {
+        cancelToken.cancel();
+      } catch (err) {}
+    };
+  }, [text, language]);
+
+  return (
+    <div>
+      <label className="label">Output</label>
+      <h1 className="title">{translated}</h1>
+    </div>
+  );
+};
